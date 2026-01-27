d2d73ca0f1b89bcda94fa9b1d92ab42aff31d21e
BIKO Yann <yann.biko@orange.com>
2026-01-27 21:38:41 +0100

    feat: add networking module (VPC, subnets, security groups)

---
+++ terraform/modules/networking/variables.tf
@@ -0,0 +1,24 @@
+variable "project_name" {
+  description = "Project name for tagging"
+  type = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_app_subnet_cidrs" {
+  description = "CIDR blocks for private app subnets"
+  type        = list(string)
+}
+
+variable "private_data_subnet_cidrs" {
+  description = "CIDR blocks for private data subnets"
+  type        = list(string)
+}
